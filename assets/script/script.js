e05d87b2e16610589154a3457bc90313bac8c0ad
jkworks <info@jilliankayworks.com>
2021-08-19 23:35:58 -0600

    still in progress sentence gen style

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,19 +9,30 @@ var scroll = window.requestAnimationFrame ||
 
 //hide name on scroll 
 
-function hideScroll() {
+// function hideScroll() {
 
-    hiJill.forEach(element => {
-        if (isElementInViewport(element)) {
-            element.classList.add('is-visible')
-        } else {
-            hiJill.setAttribute("style", "background-color: yellow;")
-        }
+//     hiJill.forEach(element => {
+//         if (isElementInViewport(element)) {
+//             element.classList.add('is-visible')
+//         } else {
+//             hiJill.setAttribute("style", "background-color: yellow;")
+//         }
         
-    });
+//     });
     
+// }
+
+var poetry1 = ["ya wild roll of the dice to decide which one of the loves of my life caused it"]
+var poetry2 = ["she jerked off her necklace i ripped under my ribs  a red mess glittered"]
+var poetry3 = ["She'll only get the best of me I'll only get the rest of her"]
+var poetry4 = ["This snake was in the grass like every snake i've scene before"]
+var poetry5 = ["learned to never walk under that bridge"]
+
+var allPoems = [poetry1, poetry2, poetry3, poetry4, poetry5]
+var sentencediv = document.getElementById('sentence')
+
+function sengen(params) {
+
 }
-
-
 
 
